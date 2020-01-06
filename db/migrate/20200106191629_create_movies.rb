459a6768435e856b42a1367535fa814dbff90911
treuther <tracyreuther@gmail.com>
2020-01-06 13:19:40 -0600

    migration creaton for movies and genres

--- db/migrate/20200106191629_create_movies.rb
+++ db/migrate/20200106191629_create_movies.rb
@@ -1,6 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
+      t.string :title
+      t.string :year
+      t.string :rating
+      t.string :description
+      t.integer :genre_id, null: false
 
       t.timestamps
     end
